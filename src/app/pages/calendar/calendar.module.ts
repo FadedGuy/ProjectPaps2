00b1e78779abff5392383e09506f02f4c3e88396
cgarciam2002 <56266086+cgarciam2002@users.noreply.github.com>
2019-10-15 14:22:01 -0500

    Calendario se muestra
    El calendario en la pagina Calendar se muestra de manera correcta

--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { CalendarPage } from './calendar.page';
+import { CalendarModule } from 'ion2-calendar';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    CalendarModule,
     RouterModule.forChild(routes)
   ],
   declarations: [CalendarPage]
