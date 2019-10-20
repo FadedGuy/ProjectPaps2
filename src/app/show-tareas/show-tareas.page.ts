e526f38e0c5733e8d66095e69a62b16ddbf21f5a
desparza13 <desparzaespinosa@gmail.com>
2019-10-20 18:17:24 -0500

    g

--- src/app/show-tareas/show-tareas.page.ts
+++ src/app/show-tareas/show-tareas.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Tareas, arrTareas, arrTareasString } from '../../allVars';
+import { Tareas, arrTareas, arrTareasString } from '../allVars';
 
 @Component({
   selector: 'app-show-tareas',
