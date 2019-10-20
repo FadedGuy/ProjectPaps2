d8b32ea0360514b529ae6024be8b6f4f5f9c5e7d
FadedGuy <42686178+Kevinazo042444@users.noreply.github.com>
2019-10-20 13:06:34 -0500

    MaN

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  imageManuel: string = "https://i.ibb.co/R0pRTPT/ICONOFINALPNG.png";
+  imagenManuel: string = "https://i.ibb.co/R0pRTPT/ICONOFINALPNG.png";
   
   constructor() {
   }
